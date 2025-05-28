2f13fed6e807bc74198d914932561a50582578de
hyeesw <hyeeun7904@kookmin.ac.kr>
2025-05-28 09:54:13 +0900

    [Add] Policy.java

---
+++ src/main/java/org/example/small_project/hyeeun/Policy.java
@@ -0,0 +1,35 @@
+package org.example.small_project.hyeeun;
+
+public class Policy {
+    private static Policy instance = new Policy();
+
+    private int maxBabyAge = 9;
+    private int maxStudentAge = 29;
+    private int minSeniorAge = 59;
+    private double babyDiscount = 0.9;
+    private double studentDiscount = 0.1;
+    private double seniorDiscount = 0.3;
+
+    private Policy() {}
+
+    public static Policy getInstance() {
+        return instance;
+    }
+
+    public void setPolicy(Passenger p) {
+        int age = p.getAge();
+        if (age <= maxBabyAge) p.setType("BABY");
+        else if (age <= maxStudentAge) p.setType("STUDENT");
+        else if (age > minSeniorAge) p.setType("SENIOR");
+        else p.setType("ORDINARY");
+    }
+
+    public double getDiscountRate(String type) {
+        return switch (type) {
+            case "BABY" -> babyDiscount;
+            case "STUDENT" -> studentDiscount;
+            case "SENIOR" -> seniorDiscount;
+            default -> 0.0;
+        };
+    }
+}

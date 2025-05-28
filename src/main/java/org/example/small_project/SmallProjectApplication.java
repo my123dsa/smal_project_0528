497b563616fb6e36a2749ffca6eeef4e024cd0f5
junhyeonpark <j0303p@gmail.com>
2025-05-28 09:26:25 +0900

    first commit

---
+++ src/main/java/org/example/small_project/SmallProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.small_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/small_project/SmallProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.small_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

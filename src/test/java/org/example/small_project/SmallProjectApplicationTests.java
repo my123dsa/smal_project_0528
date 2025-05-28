18eb68f15891fea6a706048ae54ee98f08c645c5
jhy <65846779+jhy11@users.noreply.github.com>
2025-05-28 11:00:31 +0900

    [Add] Add Test: SmallProjectApplicationTests

--- src/test/java/org/example/small_project/SmallProjectApplicationTests.java
+++ src/test/java/org/example/small_project/SmallProjectApplicationTests.java
@@ -1,13 +1,90 @@
 package org.example.small_project;
 
+import org.example.small_project.hyeeun.Policy;
+import org.example.small_project.jhy.BoardingService;
+import org.example.small_project.ljy.Passenger;
+import org.example.small_project.parkjunhyeon.domain.Bus;
+import org.example.small_project.parkjunhyeon.domain.Subway;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 class SmallProjectApplicationTests {
+
+    private Bus bus;
+    private Subway subway;
+    private BoardingService boardingService;
+
+    @BeforeAll
+     void setUp() {
+        bus = new Bus("Bus-01", 50, 1000);
+        subway = new Subway("Sub=01", 1000, 1500);
+        boardingService = new BoardingService();
+    }
+
+    // Board success
+    @Test
+    void testPassengerBoardsBusSuccessfully() {
+        // Given
+        Passenger p = new Passenger("s1", 5000, 20150909);
+        Policy.getInstance().setPolicy(p);
+        int beforeCash = p.getCash();
+
+        // When
+        boardingService.board(p, bus);
+
+        // Then
+        assertEquals("탑승", p.getState());
+        assertEquals(1, bus.getPassengers());
+        assertTrue(p.getCash() < beforeCash);
+        System.out.println();
+        assertEquals((int)(1000 * (1 - 0.1)), bus.getRevenue()); // 청소년 10% 할인
+    }
+
+
+    // Baby discount
+    @Test
+    void testBabyDiscount() {
+        // 수정
+        Passenger baby = new Passenger("baby", 5000, 20240428); // 5세
+        Policy.getInstance().setPolicy(baby);
+        int before = baby.getCash();
+
+        boardingService.board(baby, bus);
+
+        int expectedFare = (int) (1000 * (1 - 0.75)); // 90% 할인
+        System.out.println(expectedFare);
+        assertEquals("탑승", baby.getState());
+        assertEquals(before - expectedFare, baby.getCash());
+        assertEquals(expectedFare, bus.getRevenue());
+    }
+
+
+    // Max passenger
+    @Test
+    void testPassengerCannotBoard() {
+        // Given
+        Bus smallBus = new Bus("B-102", 0, 1000); // 최대인원 0
+        Passenger p = new Passenger("o1", 5000, 19900101);
+
+        // When
+        boardingService.board(p, smallBus);
+
+        // Then
+        assertEquals(null, p.getState());
+        assertEquals(0, smallBus.getPassengers());
+        assertEquals(5000, p.getCash()); // 돈도 안 빠짐
+    }
 
     @Test
     void contextLoads() {
     }
-
 }

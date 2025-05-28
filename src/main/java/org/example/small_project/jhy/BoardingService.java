24eacfb917798ee97ba28d1013f80295f0784192
jhy <65846779+jhy11@users.noreply.github.com>
2025-05-28 10:28:57 +0900

    [Add] BoardingService

---
+++ src/main/java/org/example/small_project/jhy/BoardingService.java
@@ -0,0 +1,16 @@
+package org.example.small_project.jhy;
+
+import org.example.small_project.hyeeun.Policy;
+import org.example.small_project.ljy.Passenger;
+import org.example.small_project.parkjunhyeon.domain.Vehicle;
+
+public class BoardingService {
+    public void board(Passenger p, Vehicle v) {
+        Policy.getInstance().setPolicy(p);
+        if (v.canBoard()) {
+            v.getIn(p);
+        } else {
+            System.out.println("탑승 불가: 인원 초과");
+        }
+    }
+}
